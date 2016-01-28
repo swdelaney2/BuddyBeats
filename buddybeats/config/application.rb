94b9315e5a9080c70ec7777f9093fe023b587d51
Stephen Delaney <swdelaney2@gmail.com>
2016-01-28 16:17:57 -0600

    added production url for mailer

--- buddybeats/config/application.rb
+++ buddybeats/config/application.rb
@@ -22,5 +22,8 @@ module Buddybeats
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.default_url_options = { :host => "192.241.174.8" }
+    
   end
 end
--- buddybeats/config/environments/production.rb
+++ buddybeats/config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { :host => "192.241.174.8" }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
